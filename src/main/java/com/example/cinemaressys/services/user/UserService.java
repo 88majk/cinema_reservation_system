43b92f0e9dda0e5c8be00ceaf836a112a20fce8d
gabrys0099 <jagabriello@wp.pl>
2024-05-05 21:53:29 +0200

    Jwt token

--- src/main/java/com/example/cinemaressys/services/user/UserService.java
+++ src/main/java/com/example/cinemaressys/services/user/UserService.java
@@ -2,8 +2,9 @@ package com.example.cinemaressys.services.user;
 
 import com.example.cinemaressys.dtos.user.UserLoginRequestDto;
 import com.example.cinemaressys.dtos.user.UserRegisterRequestDto;
+import com.example.cinemaressys.entities.User;
 
 public interface UserService {
     void registerUser(UserRegisterRequestDto userRegisterRequestDto);
-    void loginUser(UserLoginRequestDto userLoginRequestDto);
+    User loginUser(UserLoginRequestDto userLoginRequestDto);
 }
