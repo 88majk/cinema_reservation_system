fdca41c00ba69b2b6cc86af121f59984b7b12864
88majk <mikolaj19gr@gmail.com>
2024-06-14 19:07:44 +0200

    Pushing endpoints without gfsf

--- src/main/java/com/example/cinemaressys/configs/FirebaseConfig.java
+++ src/main/java/com/example/cinemaressys/configs/FirebaseConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.ClassPathResource;
 
-import java.io.FileInputStream;
+import java.io.FileInputStream; //
 import java.io.IOException;
 
 @Configuration
